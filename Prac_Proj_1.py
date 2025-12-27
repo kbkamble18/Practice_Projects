ea45e50e79a046f48c1148788bda2948bf778461
Karan Kamble <kbkamble18@gmail.com>
2025-12-27 13:31:37 +0530

    Add files via upload

--- Prac_Proj_1.py
+++ Prac_Proj_1.py
@@ -1,6 +1,6 @@
 # 1 - import random module
 import random
-
+"""
 # 2 - create subjects
 
 subjects =["Anant Ambani",
@@ -29,6 +29,7 @@ places_or_things = ["at Red Fort",
                    "At India Gate"
                    ]
 
+
 # 3 - start the headline generation loop
 
 while True:
@@ -39,10 +40,34 @@ while True:
     headline = f"Breaking News: {subject} {action} {Place}"
     print("\n"+ headline)
 
-    user_input = input("\n Do you want another hewadline? (yes/no)").strip().lower()
+    user_input = input("\n Do you want another headline? (yes/no)").strip().lower()
     if user_input == "no":
         break
 
 #print goodbye message
 
 print("\n Thanks for using the Fake Headline generator. Have a fun day")
+"""
+
+print("Enter subjects (enter 'done' after completion on separate new line)")
+while True:
+    sub = input("> ").strip()
+    if sub.lower()=="done":
+        break
+    subjects.append(sub)
+
+print("Enter Action after comma in single line")
+act = input(act.split(",").strip())
+
+
+print("Enter new subjects")
+sub = input()
+act = input()
+place= input()
+
+while True:
+    print("Do you want another headline?(yes/no)")
+    n = input()
+if n == "no":
+    break
+"""     
